6f422765c75d68ad7d8a09a3bf70819cf8cc020a
SS Jones <SSJones.code@gmail.com>
2020-12-02 15:28:03 -0700

    I renamed the sql file and the db favoriteThings because favorites will be a table in the db and not the db itself.

--- favoriteThings.sql
+++ favoriteThings.sql
@@ -1,4 +1,4 @@
 -- The DROP DATABASE statement is used to drop an existing SQL database.
-DROP DATABASE IF EXISTS favorites;
+DROP DATABASE IF EXISTS favoriteThings;
 -- The CREATE DATABASE statement is used to create a new SQL database.
-CREATE DATABASE favorites;
+CREATE DATABASE favoriteThings;
