2ddc54906940c06ffd0997ff80a20ad590f49fd5
SS Jones <SSJones.code@gmail.com>
2020-12-02 16:49:37 -0700

    I added the fav1, fav2, and fav3 to the favorites table in the favoriteThings.sql file as well as adding the names id from the names table to the favoriteThings table as well to connect the two tables. I added the favorite things to the seed as well along with the aforementioned names id.

--- favoriteThings.sql
+++ favoriteThings.sql
@@ -6,15 +6,17 @@ CREATE DATABASE favoriteThings;
 -- creating a table called names
 USE favoriteThings;
 CREATE TABLE names(
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
   id INT NOT NULL AUTO_INCREMENT,
-  PersonName VARCHAR(30) DEFAULT '' NOT NULL,
   PRIMARY KEY (id)
 );
 
 -- creating a table called favorites
 CREATE TABLE favorites(
-  id INT NOT NULL AUTO_INCREMENT,
-  favoriteItem VARCHAR(30) DEFAULT '' NOT NULL,
+  favorite_1 VARCHAR(30) DEFAULT '' NOT NULL,
+  favorite_2 VARCHAR(30) DEFAULT '' NOT NULL,
+  favorite_3 VARCHAR(30) DEFAULT '' NOT NULL,
   names_id INT NOT NULL,
   PRIMARY KEY (id)
 );
