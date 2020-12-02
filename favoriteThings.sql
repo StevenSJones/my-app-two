a3dd015c8c8b5f49ef5631f1833a166335750caa
SS Jones <SSJones.code@gmail.com>
2020-12-02 15:32:51 -0700

    I created the table called names that holds the personName and and id as well as a primary key.

--- favoriteThings.sql
+++ favoriteThings.sql
@@ -2,3 +2,12 @@
 DROP DATABASE IF EXISTS favoriteThings;
 -- The CREATE DATABASE statement is used to create a new SQL database.
 CREATE DATABASE favoriteThings;
+
+-- creating a table called names
+USE favoriteThings;
+CREATE TABLE names(
+  id INT NOT NULL AUTO_INCREMENT,
+  PersonName VARCHAR(30) DEFAULT '' NOT NULL,
+  PRIMARY KEY (id)
+);
+
