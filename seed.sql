35fe60fa885d65472e3b9f9b3c127b7c74f7cd58
SS Jones <SSJones.code@gmail.com>
2020-12-02 15:54:19 -0700

    I created the seed for the names table with first, last and id.

---
+++ seed.sql
@@ -0,0 +1,24 @@
+-- The CREATE DATABASE statement is used to create a new SQL database.
+USE favoriteThings;
+--The INSERT INTO statement is used here to insert rows into the names table in the order I specifiy in THIS STATEMENT BELOW
+INSERT INTO names (first_name, last_name, name_id)
+VALUES ('Moriah', 'Kincheloe', 1),
+('Geovonni', 'Banano', 2),
+('Michael', 'Corleone', 3),
+('Rusty', 'Bohnsteil', 4),
+('Shayla', 'Griffin', 5),
+('Peter', 'Luchese', 6),
+('Jada', 'Gambino', 7),
+('Fredo', 'Rodolfo', 8),
+('Lori', 'Monk', 9),
+('Fred', 'Monk', 10),
+('Penny', 'Gracie', 11),
+('Luna', 'Jane', 12),
+('Steven', 'Griffin', 13),
+('Leslee', 'Jones', 14),
+('Steven', 'Jones', 15),
+('Dwyane', 'Wade', 16),
+('Jesse', 'Stanley', 17),
+('Michael', 'Jordan', 18),
+('Hanh', 'Vu', 19),
+('Margo', 'Mendez', 20);--the VALUES inserted into the table
