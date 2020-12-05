503a421016bbfe18abd49a0cc063a45cc6257c64
SS Jones <SSJones.code@gmail.com>
2020-12-04 17:20:00 -0700

    I was able to amend the seeds and the labels so that there was an id label but not seeded data as it is generated automatically based on the data being a primary key in the names table. I did however need to add the names id to the labels in the threeFavorites table to with corresponding data so that the tables were linked.

--- schema.sql
+++ schema.sql
@@ -6,18 +6,18 @@ CREATE DATABASE favoriteThings;
 -- creating a table called names
 USE favoriteThings;
 CREATE TABLE names(
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  id INT NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (id)
 );
 
--- creating a table called favorites
+-- creating a table called threeFavorites
 CREATE TABLE threeFavorites(
+  id INT NOT NULL AUTO_INCREMENT,
   favorite_1 VARCHAR(30) DEFAULT '' NOT NULL,
   favorite_2 VARCHAR(30) DEFAULT '' NOT NULL,
   favorite_3 VARCHAR(30) DEFAULT '' NOT NULL,
-  id INT NOT NULL AUTO_INCREMENT,
   names_id INT NOT NULL,
   PRIMARY KEY (id)
 );
