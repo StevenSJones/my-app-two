e3c55bdedac78d9487cf87c2cb07fdf7891c8ac9
SS Jones <SSJones.code@gmail.com>
2020-12-05 10:19:52 -0700

    I amended the comments so as to properly reflect the work done.

--- server.js
+++ server.js
@@ -28,6 +28,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+//this is the joining of the two tables as well as the result and error.
+app.get("/favoriteThings", function(req, res){
+    const query = "select n.first_name, n.last_name, t.favorite1, t.favorite2, t.favorite3 FROM names n INNER JOIN threeFavorites t ON n.id = t.names_id";
+connection.query(query, function(result, err){
+    if (err) throw err;
+    res.send(result);
+})
+});
 //actually listening on the port for requests
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
